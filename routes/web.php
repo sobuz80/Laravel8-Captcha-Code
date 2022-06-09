e17cd44dad5cccced33acb441e56987c9e8715f3
sobuz80 <sobuz80@gmail.com>
2022-06-09 23:13:21 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CaptchaController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Route::get('contact-form', [CaptchaController::class, 'index']);
+Route::post('captcha-validation', [CaptchaController::class, 'capthcaFormValidate']);
+Route::get('reload-captcha', [CaptchaController::class, 'reloadCaptcha']);
